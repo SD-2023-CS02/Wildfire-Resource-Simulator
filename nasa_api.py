adb197aee93b06e77ff64b7ee5eae89bf4946597
Vincent Do <thevinnycast@gmail.com>
2023-10-06 21:26:46 -0700

    NASA API work + config file created

--- nasa_api.py
+++ nasa_api.py
@@ -1,12 +1,19 @@
+from configparser import ConfigParser
 import requests
 
 
-key = ""
+config = ConfigParser()
+config.read("config.ini")
+
+key = config["API"]["MapKey"]
 area = "world"
 source = "LANDSAT_NRT"
-day_range = 1
+day_range = 2
 
 url = f"https://firms.modaps.eosdis.nasa.gov/api/area/csv/{key}/{source}/{area}/{day_range}"
 res = requests.get(url)
 
-print(res.text)
+res_list = res.text.split("\n")
+
+for row in res_list[:3]:
+    print(row)
