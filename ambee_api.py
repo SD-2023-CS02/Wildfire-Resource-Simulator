bdc26ea114cadec68d9690989acb8f8dbf769459
Kevin Dang <kevinthedang_1@outlook.com>
2023-10-12 17:32:37 -0700

    if statement for console output

--- ambee_api.py
+++ ambee_api.py
@@ -33,7 +33,6 @@ if (check != 'y' and check != 'yes'):
     system.exit()
 
 print('\n>>> Querying Ambee API...\n')
-sleep(2.0)
 
 # API Key
 print('>>> Obtaining API Key Locally...')
@@ -51,6 +50,10 @@ except:
 lat = 47.668331
 long = -117.402771
 
+# South of Kennewick Fire
+lat = 45.880782
+long = -119.167000
+
 # obtain query type -> known: latest, forecast
 type_check = ''
 while (type_check != 'fire' and type_check != 'general'):
@@ -58,14 +61,11 @@ while (type_check != 'fire' and type_check != 'general'):
 
 query_type = ''
 
-if (type_check == 'fire'):
-    query_type = 'latest'
-
 while (query_type != 'latest' and query_type != 'forecast'):
     query_type = input('\n>>> What kind of information are you querying? (\'latest\', \'forecast\'): ')
 
 fire = f'fire/v2/{query_type}/by-lat-lng'
-normal = f'{query_type}/by-lat-lng'
+normal = f'weather/{query_type}/by-lat-lng'
 
 query = ''
 if (type_check == 'fire'):
@@ -77,6 +77,10 @@ else:
 print(f'\n>>> Attempting to Query {type_check} data from https://api.ambeedata.com/...')
 sleep(2.0)
 
+# sample = b'{"message":"success","stations":[{"CO":0.153,"NO2":5.722,"OZONE":28.03,"PM10":9.361,"PM25":3.17,"SO2":1.288,"city":"Spokane","countryCode":"US","division":"Spokane","lat":47.64999999999219,"lng":-117.39999999998577,"placeName":"E Hartson Ave","postalCode":"99202-2334","state":"Washington","updatedAt":"2023-10-11T19:00:00.000Z","AQI":26,"aqiInfo":{"pollutant":"O3","concentration":28.03,"category":"Good"}}]}'
+# with open('ambee_general.json', 'w') as fun:
+#         json.dump(json.loads(sample)['stations'], fun, indent=3)
+
 url = f"https://api.ambeedata.com/{query}?lat={lat}&lng={long}"
 header = {'x-api-key' : key}
 
@@ -85,10 +89,20 @@ request_api = request.Request(url=url, headers=header)
 response_api = request.urlopen(request_api)
 
 # format received byte string data
-print('\n>>> Formatting Data...')
+print('\n>>> Formatting Data...\n')
 sleep(2.0)
+data = response_api.read()
+json_data = json.loads(data)
+print(data)
+
 if (type_check == 'fire'):
-    print(f'\n{response_api.read()}')
+    print(f'\n{data}\n')
+    with open('ambee_fire_forecast.json', 'w') as fun:
+        json.dump(json_data, fun, indent=3)
     # print(json.dumps(json.loads(response_api.read()), indent=3)) -> not working right now
 else:
-    print_results(json.loads(response_api.read())['stations'])
+    if (type_check == 'general' and query_type == 'latest'):
+        print_results(json_data['stations'])
+    # write to .json
+    with open('ambee_general.json', 'w') as fun:
+        json.dump(json_data, fun, indent=3)
