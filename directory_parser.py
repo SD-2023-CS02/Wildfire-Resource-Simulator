23edc13f8f8252705cfd7e668143977d577942a2
Vincent Do <thevinnycast@gmail.com>
2023-11-16 15:14:48 -0800

    Minor module adjustments

--- directory_parser.py
+++ directory_parser.py
@@ -1,5 +1,6 @@
 from py_pdf_parser.loaders import load_file
 
+
 class DirectoryParser:
     def __init__(self, file_path, folder="output"):
         self.file_path = file_path
@@ -7,13 +8,15 @@ class DirectoryParser:
         self.pages = []
         self.load_data()
 
+
     def load_data(self):
-        doc = load_file("data/air_tanker_base_directory.pdf")
+        doc = load_file(self.file_path)
         elements = doc.elements
 
         with open(f"{self.folder}/elements.txt", "w", errors="ignore") as f:
             for element in elements:
                 f.write(element.text() + "\n")
+
 
     def process_page(self, page_data):
         base_info = {}
@@ -21,7 +24,9 @@ class DirectoryParser:
         base_info['Airport'] = page_data[1]
         base_info['Latitude,Longitude'] = page_data[2]
         base_info['Base Location on Field'] = page_data[3]
+
         return base_info
+
 
     def parse_directory(self):
         with open(f'{self.folder}/elements.txt', 'r') as file:
@@ -29,15 +34,18 @@ class DirectoryParser:
 
         pages = []
         current_page = []
+
         for line in data:
-            current_page.append(line)
             if 'of 144' in line:
-                pages.append(current_page[:-1])  # Remove the 'of 144' line
+                pages.append(current_page)  # excludes 'of 144' line
                 current_page = []
+            else:
+                current_page.append(line)
 
         for idx, page in enumerate(pages, start=1):
             base_info = self.process_page(page)
             print(f"Base {idx} Information:")
+
             for key, value in base_info.items():
                 print(f"{key}: {value}")
             print("\n")
