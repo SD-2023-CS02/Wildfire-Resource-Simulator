3aa711b68d52a21179d92787ee0dcdf8f6fa31ea
Claire Yegian <claireyegian@gmail.com>
2023-12-01 16:01:16 -0800

    added notes for remaining necessary variables

--- directory_parser.py
+++ directory_parser.py
@@ -1,5 +1,15 @@
 from py_pdf_parser.loaders import load_file
 
+# Retrieve Base Name index 0
+# Latitude longitude index 2
+# Geographic area? index 3 after "Base Location"
+# Runway weight limits: single, dual, 2S, 2D 
+#   if there's not a phone number before "Runway weight limits"... collect all numbers between each subtitle (single, double, etc)
+# VLATs, Pit Total, Hot Loading, LATs, Parking Total, Fuel and Load, MAFFS, Load Simultaneously, 
+#   Hot Refueling, SEATs, Offload Capacity, Reserved
+#   parse values between titles ... if dne the other titles, make sure there are enough to sort,
+# Retardant Jettison Area(s)
+#   if it contains 'Lat' and 'Long' or numbers and 'N' and 'W'
 
 class DirectoryParser:
     def __init__(self, file_path, folder="output"):
@@ -17,13 +27,25 @@ class DirectoryParser:
             for element in elements:
                 f.write(element.text() + "\n")
 
+    #def get_runway_weight_lims(self):
+        # Runway weight limits: single, dual, 2S, 2D 
+        #   if there's not a phone number before "Runway weight limits"... collect all numbers between each subtitle (single, double, etc)
+
+    #def get_load_limits(self):
+        # VLATs, Pit Total, Hot Loading, LATs, Parking Total, Fuel and Load, MAFFS, Load Simultaneously, 
+        #   Hot Refueling, SEATs, Offload Capacity, Reserved
+        #   parse values between titles ... if dne the other titles, make sure there are enough to sort,
+
+    #def get_jettison_area(self):
+        # Retardant Jettison Area(s)
+        #   if it contains 'Lat' and 'Long' or numbers and 'N' and 'W'
 
     def process_page(self, page_data):
         base_info = {}
         base_info['Base Name'] = page_data[0].split('�')[0]
         base_info['Airport'] = page_data[1]
         base_info['Latitude,Longitude'] = page_data[2]
-        base_info['Base Location on Field'] = page_data[3]
+        base_info['Geographic Area'] = page_data[4]
 
         return base_info
 
