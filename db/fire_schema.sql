7a7b64bc42a933d3d93c35a777999d662ed7de60
Vincent Do <thevinnycast@gmail.com>
2024-02-14 22:18:28 -0800

    Updated FirePoint schema

--- db/fire_schema.sql
+++ db/fire_schema.sql
@@ -13,3 +13,28 @@ CREATE TABLE FirePoint (
     incident_size_acres FLOAT(12, 3),
     PRIMARY KEY (create_date, latitude, longitude)
 );
+
+CREATE TABLE FirePoint (
+    fire_id VARCHAR(50),
+    incident_name VARCHAR(50) NOT NULL,
+    gacc VARCHAR(10) NOT NULL,
+    create_date DATETIME NOT NULL,
+    latitude DECIMAL(16, 13) NOT NULL,
+    longitude DECIMAL(16, 13) NOT NULL,
+    -- Optional Location Data
+    initial_lat DECIMAL(16, 13),
+    initial_long DECIMAL(16, 13),
+    -- Optional Time Data
+    containment_date DATETIME,
+    fireout_date DATETIME,
+    control_date DATETIME,
+    discovery_date DATETIME,
+    response_date DATETIME,
+    -- Optional Acre Data
+    discovery_acres FLOAT(12,3),
+    incident_size_acres FLOAT(12, 3),
+    final_acres FLOAT(12, 3),
+    initial_acres FLOAT(12, 3),
+    -- Table Keys
+    PRIMARY KEY (fire_id)
+);
