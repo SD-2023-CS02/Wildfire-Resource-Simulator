8bb6a1204f8b9a6d33c410c6b71de72e25f5422a
Vincent Do <thevinnycast@gmail.com>
2024-02-18 15:29:36 -0800

    FirePoint insert gen script updated for new table schema

--- db/fire_schema.sql
+++ db/fire_schema.sql
@@ -3,33 +3,20 @@ DROP TABLE IF EXISTS FirePoint;
 
 -- Create Tables
 CREATE TABLE FirePoint (
-    create_date DATETIME,
-    latitude DECIMAL(16, 13),
-    longitude DECIMAL(16, 13),
-    -- Non-Key Fire Data
-    containment_date DATETIME,
-    fireout_date DATETIME,
-    discovery_acres FLOAT(12,3),
-    incident_size_acres FLOAT(12, 3),
-    PRIMARY KEY (create_date, latitude, longitude)
-);
-
-CREATE TABLE FirePoint (
     fire_id VARCHAR(50),
     incident_name VARCHAR(50) NOT NULL,
-    gacc VARCHAR(10) NOT NULL,
     create_date DATETIME NOT NULL,
+    discovery_date DATETIME NOT NULL,
     latitude DECIMAL(16, 13) NOT NULL,
     longitude DECIMAL(16, 13) NOT NULL,
     -- Optional Location Data
     initial_lat DECIMAL(16, 13),
     initial_long DECIMAL(16, 13),
+    gacc VARCHAR(10),
     -- Optional Time Data
     containment_date DATETIME,
     fireout_date DATETIME,
     control_date DATETIME,
-    discovery_date DATETIME,
-    response_date DATETIME,
     -- Optional Acre Data
     discovery_acres FLOAT(12,3),
     incident_size_acres FLOAT(12, 3),
--- fire_inserts.py
+++ fire_inserts.py
@@ -13,26 +13,41 @@ OUT_FILE = 'fire_data'
 TABLE = 'FirePoint'
 
 NULL = 'NULL'
-KEYS = ['CreatedOnDateTime_dt', 'Latitude', 'Longitude']
+NAN = 'nan'
+#KEYS = ['CreatedOnDateTime_dt', 'Latitude', 'Longitude']
 
 
 fire_df = pd.read_csv(f'{IN_FOLDER}/{IN_FILE}.csv')
-fire_df.drop_duplicates(subset=KEYS, keep='first', inplace=True) # temporary solution for dropping duplicate incident locations+dates
+#fire_df.drop_duplicates(subset=KEYS, keep='first', inplace=True) # temporary solution for dropping duplicate incident locations+dates
 
 
-with open(f'{OUT_FOLDER}/{OUT_FILE}.sql', 'w') as f:
+with open(f'{OUT_FOLDER}/{OUT_FILE}.sql', 'w', errors='ignore') as f:
     f.write(f'INSERT INTO {TABLE} VALUES\n')
-
+    
     for i in range(len(fire_df)):
         row = fire_df.iloc[i]
         
-        containment_time = f'\'{row["ContainmentDateTime"][:-6]}\'' if row['ContainmentDateTime'] is not np.nan else NULL
-        fire_out_time = f'\'{row["FireOutDateTime"][:-6]}\'' if row["FireOutDateTime"] is not np.nan else NULL
-        discovery_acres = row['DiscoveryAcres'] if str(row['DiscoveryAcres']) != 'nan' else NULL
-        incident_size = row['IncidentSize'] if str(row['IncidentSize']) != 'nan' else NULL
+        fire_id = str(row['UniqueFireIdentifier']).replace('\'', '\\\'').replace('"', '\\"')
+        incident_name = str(row['IncidentName']).replace('\'', '\\\'').replace('"', '\\"')
 
-        s = (f'  (\'{row["CreatedOnDateTime_dt"][:-6]}\', {row["Latitude"]}, {row["Longitude"]}, ' +
-             f'{containment_time}, {fire_out_time}, {discovery_acres}, {incident_size})')
+        initial_lat = f'\'{row["InitialLatitude"]}\'' if str(row['InitialLatitude']) != NAN else NULL
+        initial_long = f'\'{row["InitialLongitude"]}\'' if str(row['InitialLongitude']) != NAN else NULL
+        gacc = f'\'{row["GACC"]}\'' if str(row['GACC']) != NAN else NULL
+
+        containment_date = f'\'{row["ContainmentDateTime"][:-6]}\'' if row['ContainmentDateTime'] is not np.nan else NULL
+        fireout_date = f'\'{row["FireOutDateTime"][:-6]}\'' if row["FireOutDateTime"] is not np.nan else NULL
+        control_date = f'\'{row["ControlDateTime"][:-6]}\'' if row["ControlDateTime"] is not np.nan else NULL
+
+        discovery_acres = row['DiscoveryAcres'] if str(row['DiscoveryAcres']) != NAN else NULL
+        incident_size = row['IncidentSize'] if str(row['IncidentSize']) != NAN else NULL
+        final_acres = row['FinalAcres'] if str(row['FinalAcres']) != NAN else NULL
+        initial_acres = row['InitialResponseAcres'] if str(row['InitialResponseAcres']) != NAN else NULL
+
+        s = f'  (\'{fire_id}\', \'{incident_name}\','
+        s += f' \'{row["CreatedOnDateTime_dt"][:-6]}\', \'{row["FireDiscoveryDateTime"][:-6]}\','
+        s += f' {row["Latitude"]}, {row["Longitude"]}, {initial_lat}, {initial_long}, {gacc},'
+        s += f' {containment_date}, {fireout_date}, {control_date},'
+        s += f' {discovery_acres}, {incident_size}, {final_acres}, {initial_acres})'
         s += (',' if i < (len(fire_df) - 1) else ';') + '\n'
         
         f.write(s)
