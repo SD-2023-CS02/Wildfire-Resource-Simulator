f320f7d4c04afe9baa1562806d261c960c82a2a7
Vincent D <51691886+VinnyVinVince@users.noreply.github.com>
2024-03-11 11:03:41 -0700

    Tanker Base Data (#2)
    * Air Tanker Base Directory added + branching
    
    * Testing PDF Parser library on Directory
    
    * added manual parser for some basic base info. turned directory_parser into a class and started instantiating and running in main.py
    
    * Minor module adjustments
    
    * Removed newlines from parsed pages
    
    * added notes for remaining necessary variables
    
    * fixed runway weight limits method and added load limits method
    
    * file read/write encoding fix for windows
    
    * tanker base now parses to pdf. Added code to write sql script for inserts. Needs work
    
    * Insert statement value correction
    
    * Table renaming
    
    * Directory parser to csv update
    
    * working on finding missing data
    
    * Tanker Base CSV based on manual entry
    
    * TankerBase table schema
    
    * TankerBase table docs
    
    * Updated TankerBase insert statement gen script
    
    * Fixing longitude
    
    * fixed ramona airport
    
    ---------
    
    Co-authored-by: Claire Yegian <claireyegian@gmail.com>
    Co-authored-by: Kevin Dang <kevinthedang_1@outlook.com>

---
+++ db/tankerbase_schema.sql
@@ -0,0 +1,32 @@
+DROP TABLE IF EXISTS TankerBase;
+
+CREATE TABLE TankerBase (
+    base_code CHAR(3), -- FAA ID
+    base_name VARCHAR(50) NOT NULL,
+    airport VARCHAR(50) NOT NULL,
+    region VARCHAR(50) NOT NULL,
+    elevation INT UNSIGNED NOT NULL, -- ft
+    latitude DECIMAL(16, 13) NOT NULL,
+    longitude DECIMAL(16, 13) NOT NULL,
+    -- Runway Weight Limits (tons)
+    single_rwl FLOAT NOT NULL,
+    double_rwl FLOAT NOT NULL,
+    2s_rwl FLOAT NOT NULL,
+    2d_rwl FLOAT NOT NULL,
+    -- Allowed Airtanker Types
+    vlat ENUM('YES', 'NO') NOT NULL, -- very large
+    lat ENUM('YES', 'NO') NOT NULL, -- large
+    seat ENUM('YES', 'NO') NOT NULL, -- single engine
+    -- Misc
+    maffs ENUM('YES', 'NO', 'R', 'H', 'F', 'R/H/F', 'R/H', 'H/F', 'R/F') NOT NULL,
+    hot_load ENUM('YES', 'NO') NOT NULL,
+    fuel_load ENUM('YES', 'NO') NOT NULL,
+    hot_refuel ENUM('YES', 'NO') NOT NULL,
+    retardant_type ENUM('NONE', 'LC 95A', 'MVP FX', 'P-100') NOT NULL,
+    pit_total INT UNSIGNED NOT NULL,
+    parking_total INT UNSIGNED NOT NULL,
+    load_simul INT UNSIGNED NOT NULL,
+    offload_capacity INT UNSIGNED NOT NULL, -- gallons
+    -- Keys
+    PRIMARY KEY (base_code)
+);
