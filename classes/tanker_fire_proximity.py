f0d2fddefa93eea1ebe0c3799ae7c8fd179b65d8
Vincent Do <thevinnycast@gmail.com>
2024-05-04 14:22:34 -0700

    Repository restructuring for packaging

--- classes/tanker_fire_proximity.py
+++ classes/tanker_fire_proximity.py
@@ -1,4 +1,3 @@
-import pandas as pd
 import numpy as np
 from haversine import haversine, Unit
 # NOTE: must >>pip install haversine<< to run this code
--- tankerbase_visits.sql
+++
@@ -1,4 +0,0 @@
-CREATE VIEW TankerBaseVisits AS
-SELECT DISTINCT f.flight_id, f.tail_no, t.base_code
-FROM Flight f CROSS JOIN TankerBase t
-WHERE ROUND(f.latitude, 1) = ROUND(t.latitude, 1) AND ROUND(f.longitude, 1) = ROUND(t.longitude, 1);
